b0df872b95f2871adc9221262efe6580a9460b33
Bazargeldiyev Renat <xa9e@protonmail.com>
2020-09-11 23:07:35 +0600

    navigation, styling implemented, first fragment is kind of ready

--- app/src/main/java/kz/nan/vkvezdekod/MainActivity.kt
+++ app/src/main/java/kz/nan/vkvezdekod/MainActivity.kt
@@ -1,11 +1,20 @@
 package kz.nan.vkvezdekod
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+        window.navigationBarColor = ContextCompat.getColor(this, R.color.colorNavBar)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.colorStatusBar)
+        //context.getWindow().setNavigationBarColor(ContextCompat.getColor(context, R.color.navBar));
+        // context.getWindow().setStatusBarColor(ContextCompat.getColor(context,R.color.statusBar));
     }
 }
--- app/src/main/java/kz/nan/vkvezdekod/screens/start/StartFragment.kt
+++ app/src/main/java/kz/nan/vkvezdekod/screens/start/StartFragment.kt
@@ -1,10 +1,12 @@
 package kz.nan.vkvezdekod.screens.start
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import kz.nan.vkvezdekod.R
 
 class StartFragment : Fragment() {
@@ -14,6 +16,13 @@ class StartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        val view = inflater.inflate(R.layout.fragment_start, container, false)
+        val button = view.findViewById<FrameLayout>(R.id.button_create_donation)
+        button.setOnClickListener {
+            //Toast.makeText(this, "Hi", Toast.LENGTH_SHORT).show()
+            Toast.makeText(activity, "hi", Toast.LENGTH_SHORT).show()
+        }
+
+        return view
     }
 }
